d576be78c7b86fc8740df8f3e9606cbfd664b35d
hikmet <aydin96tr@gmail.com>
2024-01-02 23:29:09 +0300

    add /dashboard and /calendar

---
+++ app/calendar/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Calendar() {
+  // todo: find a calendar
+  return <>Calendar</>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,12 @@
-import { Card, Grid, Tab, TabGroup, TabList, TabPanel, TabPanels, Text, Title } from "@tremor/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4">
-      <div className="w-[30%] bg-gray-400">Sidebar</div>
-      <div className="w-full">
-        <Title>Dashboard</Title>
-        <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
-        <TabGroup className="mt-6">
-          <TabList>
-            <Tab>Page 1</Tab>
-            <Tab>Page 2</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-6">
-                <Card>
-                  {/* Placeholder to set height */}
-                  <div className="h-28" />
-                </Card>
-                <Card>
-                  {/* Placeholder to set height */}
-                  <div className="h-28" />
-                </Card>
-                <Card>
-                  {/* Placeholder to set height */}
-                  <div className="h-28" />
-                </Card>
-              </Grid>
-              <div className="mt-6">
-                <Card>
-                  <div className="h-80" />
-                </Card>
-              </div>
-            </TabPanel>
-            <TabPanel>
-              <div className="mt-6">
-                <Card>
-                  <div className="h-96" />
-                </Card>
-              </div>
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
-      </div>
-    </div>
+    <>
+      Welcome to your Dashboard, please{" "}
+      <Link href={`/dashboard`}>
+        <button className="bg-gray-600 p-4">Login</button>
+      </Link>
+    </>
   );
 }
