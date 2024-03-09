361a81b862e76ed020011dee5894c0cd5f6de28e
hikmet <aydin96tr@gmail.com>
2024-03-09 17:25:48 +0300

    prepare for caching

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,25 @@
 import { Card } from "@tremor/react";
+import { kv } from "@vercel/kv";
 
-export default function Profile() {
+export default async function Profile() {
+  // Acts like user comes from anywhere
+  await kv.hset("user", {
+    name: "Some Name",
+    age: 24,
+    email: "a@b.c",
+  });
+  // Reads value from cache
+  const user = await kv.hgetall("user");
+  console.log("User: ", user);
+
   return (
     <div className="m-2 h-full p-2">
       <Card>
         <h3>Profile</h3>
-        <p>Profile info for User</p>
+        <p>
+          Profile info for User <br />
+          {user.name} {user.age} {user.email}
+        </p>
       </Card>
     </div>
   );
