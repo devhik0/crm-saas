121a20de4d9fb5a3fa9c4dce2be5fddd43476727
hikmet <aydin96tr@gmail.com>
2024-05-27 19:44:57 +0300

    add bundle analyzer

--- client-app/next.config.js
+++ client-app/next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer(nextConfig);
