70f3fbfdf8c56312342b84b36d4a2752d4e6902b
hikmet <aydin96tr@gmail.com>
2024-07-23 02:33:53 +0300

    add auth and middleware

--- client-app/app/layout.tsx
+++ client-app/app/layout.tsx
@@ -1,13 +1,14 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { BellIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
-import { Switch } from "@tremor/react";
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { BellIcon } from "@heroicons/react/24/outline";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ReactNode } from "react";
 import "./globals.css";
-import LandingPage from "./landing-page";
+import { Providers } from "./providers/providers";
 import Sidebar from "./side-bar";
+import Theme from "./theme";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "800"] });
 
@@ -17,46 +18,41 @@ export const metadata: Metadata = {
     "CustomerNexus is an innovative CRM (Customer Relationship Management) software designed to streamline your business operations and foster stronger connections with your clients. This powerful app enables you to manage and analyze customer interactions, track sales opportunities, and optimize your marketing strategies all in one centralized platform.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const login = true;
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
-        {login ? (
-          <div className="flex flex-row">
-            <Sidebar />
-            <div className="w-full">
-              <div className="flex w-full items-center justify-between bg-gray-900 p-3">
-                <div className="flex w-full max-w-sm items-center space-x-2 rounded-md bg-gray-900 p-2">
-                  <Input type="search" placeholder="Search in all your data..." />
-                  <Button type="submit" className="bg-gray-800">
-                    Search
-                  </Button>
-                </div>
-                <div className="flex items-center gap-6">
-                  <div className="flex flex-row items-center gap-1">
-                    <SunIcon className="size-5" />
-                    <Switch checked={true} />
-                    <MoonIcon className="size-4" />
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${poppins.className} dark:bg-gray-900`}>
+          <Providers>
+            <div className="flex flex-row bg-gray-100 dark:bg-gray-900 dark:text-gray-100">
+              <Sidebar />
+              <div className="w-full">
+                <div className="flex w-full items-center justify-between p-3 dark:bg-gray-900">
+                  <div className="flex w-full max-w-sm items-center space-x-2 rounded-md p-2 dark:bg-gray-900">
+                    <Input type="search" placeholder="Search in all your data..." />
+                    <Button type="submit" className="dark:bg-gray-800">
+                      Search
+                    </Button>
                   </div>
-                  <BellIcon className="size-5" />
-                  <div className="flex flex-row items-center gap-2 border-l-2 border-l-gray-800 p-2">
-                    <Avatar>
-                      <AvatarImage src="https://github.com/shadcn.png" />
-                      <AvatarFallback>U</AvatarFallback>
-                    </Avatar>
-                    <span>User1</span>
+                  <div className="flex items-center gap-6">
+                    <Theme />
+                    <BellIcon className="size-5" />
+                    <div className="flex flex-row items-center gap-2 border-l-2 border-l-gray-800 px-4">
+                      <SignedOut>
+                        <SignInButton />
+                      </SignedOut>
+                      <SignedIn>
+                        <UserButton />
+                      </SignedIn>
+                    </div>
                   </div>
                 </div>
+                {children}
               </div>
-              {children}
             </div>
-          </div>
-        ) : (
-          <LandingPage />
-        )}
-      </body>
-    </html>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
